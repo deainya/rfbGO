f60611255eb10bbfba4f168c6c42662599a92b7f
deainya <deainya@gmail.com>
2016-10-16 18:59:53 +0400

    try

--- client/scripts/controllers/Profile.js
+++ client/scripts/controllers/Profile.js
@@ -1,9 +1,10 @@
 // User profile controller
-module.exports = function ($scope, get, Entity, Gravatar) {
+module.exports = function ($scope, $rootScope, get, Entity, Gravatar) {
   // Log getObject when controller executes
   //console.log(get);
   // Assign getObject to $scope
   $scope.profile = get.data;
   $scope.gravatarUrl = Gravatar.generate(get.data.email, 80);
+  $rootScope.role = get.data.role;
   Entity.set(get.data);
 };
