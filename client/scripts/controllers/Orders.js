0257f63d7db9e10e96251edba56c00ea8da2aeca
deainya <deainya@gmail.com>
2016-10-16 21:52:31 +0400

    Ok

--- client/scripts/controllers/Orders.js
+++ client/scripts/controllers/Orders.js
@@ -7,7 +7,7 @@ module.exports = function ($scope, $state, get, dataSource) {
 
   $scope.Create = function(neworder){
     angular.extend(neworder, {partner:get}, {"status":"Новый"});
-    delete neworder.role;
+    delete neworder.partner.role;
     console.log(neworder);
     dataSource.set('/orders/create', neworder).then(function(){
       $state.go("orders");
