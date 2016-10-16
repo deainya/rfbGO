cb0eb36a27586aec820df61d22a8038b297015a1
deainya <deainya@gmail.com>
2016-10-16 21:33:22 +0400

    try

--- client/scripts/controllers/Orders.js
+++ client/scripts/controllers/Orders.js
@@ -15,21 +15,14 @@ module.exports = function ($scope, $state, get, dataSource) {
 
   $scope.changeStatus = function(state, orderid){
     var url ='';
-    console.log(state);
     switch(state){
-      case "Отменить":
-        url = '/orders/cancel';
-        break;
-      case "Принять":
-        url = '/orders/accept';
-        break;
-      case "Завершить":
-        url = '/orders/resolve';
-      default:
-        console.log("ouch");
+      case "Отменить": url = '/orders/cancel'; break;
+      case "Принять": url = '/orders/accept'; break;
+      case "Завершить": url = '/orders/resolve'; break;
+      default: console.log("ouch");
     }
     console.log(url);
-    if (length(url)>0) {
+    if (!!url) {
       dataSource.set(url, orderid).then(function(){
         console.log(orderid);
         $state.reload();
@@ -38,9 +31,6 @@ module.exports = function ($scope, $state, get, dataSource) {
   };
 
   $scope.isCancel = function(status){ return status === "Новый" || status === "Принят" }; //show
-
   $scope.isAccept = function(status){ return status === "Новый" }; //show
-
   $scope.isResolve = function(status){ return status === "Принят" }; //show
-
 };
