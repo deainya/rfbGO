338bb1bc70f7028f2026c71bf820c515ae13cd03
deainya <deainya@gmail.com>
2016-11-14 01:36:10 +0400

    fix

--- client/scripts/controllers/Signup.js
+++ client/scripts/controllers/Signup.js
@@ -1,7 +1,6 @@
 // Signup controller
 module.exports = function ($scope) {
-  $scope.Register = function(neworder){
-    //var entity = Entity.get();
+  $scope.Register = function(user){
     angular.extend(user, {created:new Date()});
     dataSource.set('/auth/signup', user).then(function(){
       $state.go("profile");
