0030cc757d1a92f1b03cfb667c6229883dd4e1aa
deainya <deainya@gmail.com>
2016-11-20 01:42:10 +0400

    try

--- client/scripts/controllers/Orders.js
+++ client/scripts/controllers/Orders.js
@@ -6,10 +6,10 @@ module.exports = function ($scope, $state, get, dataSource, Entity) {
 
   $scope.orders = get.data;
 
-  $scope._from = new Date();
+  /*$scope._from = new Date();
   $scope._from.setHours(0, 0, 0, 0);
   $scope._to = new Date() + 1;
-  $scope._to.setHours(0, 0, 0, 0);
+  $scope._to.setHours(0, 0, 0, 0);*/
 
   $scope.Filter = function(){
     dataSource.getFiltered('/orders', {from: $scope._from, to: $scope._to});
