3111d47eaa8a36a22d6eb59a7fb29d7972f51948
deainya <deainya@gmail.com>
2016-11-21 23:18:51 +0400

    tr

--- client/scripts/services/Session.js
+++ client/scripts/services/Session.js
@@ -2,8 +2,9 @@
 module.exports = function ($log, localStorage) {
   // Instantiate data when service is loaded
   this._user = JSON.parse(localStorage.getItem('session.user'));
-  //console.log(localStorage.getItem('session.accessToken'));
   this._accessToken = localStorage.getItem('session.accessToken');
+  console.log(this._user);
+  console.log(this._accessToken);
   return {
     getUser: function(){
       return this._user;
