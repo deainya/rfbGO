f8bfce2de2128705fa270534b46e48f2470e053a
deainya <deainya@gmail.com>
2016-11-28 08:33:14 +0400

    fix

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,7 @@
 import angular from 'angular'
 import 'angular-ui-router'
 import 'angular-resource'
-import 'cordova'
+//import 'cordova'
 
 var Auth = require('./services/Auth');
 var Session = require('./services/Session');
