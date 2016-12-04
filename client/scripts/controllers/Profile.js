f4dd617828c1f7ca8e98d1428f34081b88237eae
deainya <deainya@gmail.com>
2016-12-04 21:55:27 +0400

    fix 10

--- client/scripts/controllers/Profile.js
+++ client/scripts/controllers/Profile.js
@@ -9,44 +9,40 @@ module.exports = function ($rootScope, $scope, $state, auth, dataSource, Gravata
     $state.go('profile');
   };
 
-  if(auth.isLoggedIn()){
-    $scope.tradepoints = false;
+  $scope.tradepoints = false;
 
-    // Get avatar for User from Gravatar API
-    $scope.gravatarUrl = Gravatar.generate($rootScope.user.email, 80); //???
+  // Get avatar for User from Gravatar API
+  $scope.gravatarUrl = Gravatar.generate($rootScope.user.email, 80); //???
 
-    // is User at work?
-    $scope.atWork = function(){
-      $rootScope.user.atWork = !$rootScope.user.atWork;
-      if ($rootScope.user.atWork) {
-        var obj = {action:"На работе", name: $rootScope.user.name, email: $rootScope.user.email, created:new Date()};
-      } else {
-        var obj = {action:"Не на работе", name: $rootScope.user.name, email: $rootScope.user.email, created:new Date()};
+  // is User at work?
+  $scope.atWork = function(){
+    $rootScope.user.atWork = !$rootScope.user.atWork;
+    if ($rootScope.user.atWork) {
+      var obj = {action:"На работе", name: $rootScope.user.name, email: $rootScope.user.email, created:new Date()};
+    } else {
+      var obj = {action:"Не на работе", name: $rootScope.user.name, email: $rootScope.user.email, created:new Date()};
+    }
+    dataSource.set('/api/user/atwork', obj).then(function(){
+      console.log(obj);
+    });
+    //navigator.notification.beep(2000);
+  };
+
+  // get/set Tradepoint for User
+  $scope.setTradepoint = function(obj){
+    dataSource.set('/api/user/tradepoint', {email: $rootScope.user.email, tradepoint: obj}).then(function(res){
+      if (res.data.success) {
+        $rootScope.user.tradepoint = obj;
+        $state.reload();
       }
-      dataSource.set('/api/user/atwork', obj).then(function(){
-        console.log(obj);
-      });
-      //navigator.notification.beep(2000);
-    };
-
-    // get/set Tradepoint for User
-    $scope.setTradepoint = function(obj){
-      dataSource.set('/api/user/tradepoint', {email: $rootScope.user.email, tradepoint: obj}).then(function(res){
-        if (res.data.success) {
-          $rootScope.user.tradepoint = obj;
-          $state.reload();
-        }
-      });
-    };
-    $scope.getTradepoints = function(){
-      $scope._tradepoints = true;
-      dataSource.get('/api/tradepoints', {city: $rootScope.user.city}).then(function(res) {
-        $scope.points = res.data;
-      });
-    };
-  } else {
-    $state.go('login');
-  }
+    });
+  };
+  $scope.getTradepoints = function(){
+    $scope._tradepoints = true;
+    dataSource.get('/api/tradepoints', {city: $rootScope.user.city}).then(function(res) {
+      $scope.points = res.data;
+    });
+  };
 };
 
 // OLD CODE
