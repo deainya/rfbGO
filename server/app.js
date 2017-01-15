195839216962ea14286020b339e911c15b13d826
deainya <deainya@gmail.com>
2017-01-16 02:03:02 +0400

    try

--- server/app.js
+++ server/app.js
@@ -125,10 +125,12 @@ apiRoutes.get("/tradepoints", (req, res) => {
   let city = req.query.city || {};
   let role = req.query.role || {};
   let tradepoints = Mongo.tradepoints();
-
   if (role == 0) {
     tradepoints.aggregate([{$match : {"city":city}}, {$group : { _id : {wp:"$wp", tradepoint:"$tradepoint", address:"$address", city:"$city"} }}]).toArray((err, docs) => {
       if(err) { res.sendStatus(400); }
+
+      console.log( docs );
+
       res.json( docs );
     });
   } else{
