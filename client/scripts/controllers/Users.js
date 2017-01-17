ed9d9664904a769dd7045ea9f09dea26ff1f2d37
deainya <deainya@gmail.com>
2017-01-18 00:44:09 +0400

    fix

--- client/scripts/controllers/Users.js
+++ client/scripts/controllers/Users.js
@@ -1,6 +1,6 @@
 // Users controller
-module.exports = function ($rootScope, $scope, $state, dataSource) {
-  dataSource.get('/api/userslonglist', null).then(function(res) {
+module.exports = function ($rootScope, $scope, dataSource) {
+  dataSource.get('/api/userslonglist', {}).then(function(res) {
     $scope.users = res.data;
   });
 };
