14830fa081e7e324ffca68a1296a4727ce1503e5
deainya <deainya@gmail.com>
2017-01-28 11:28:40 +0400

    fuck yeah

--- server/configExpress.js
+++ server/configExpress.js
@@ -4,11 +4,14 @@ let bodyParser  = require('body-parser'); // will let us get parameters from our
 let morgan      = require('morgan'); // will log requests to the console so we can see what is happening
 
 module.exports = function(app, express) {
-  let jsonParser = bodyParser.json();
-
   app.use(bodyParser.json()); // get our request parameters
   app.use(bodyParser.urlencoded({ extended: false }));
   //app.use(express.cookieParser()); // read cookies (needed for auth)
   app.use(morgan('dev')); // use morgan to log requests to the console
   app.use( express.static(__dirname + "/../client") ); // default route
+
+  jsonParser() {
+    return bodyParser.json();
+  }
+
 }
