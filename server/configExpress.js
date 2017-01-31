ea00398f4c6fd95d539cc9e65e0d8385145af054
deainya <deainya@gmail.com>
2017-02-01 01:49:20 +0400

    exclude morgan

--- server/configExpress.js
+++ server/configExpress.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let morgan      = require('morgan'); // will log requests to the console so we can see what is happening
+//let morgan      = require('morgan'); // will log requests to the console so we can see what is happening
 
 module.exports = function(app, express, bodyParser) {
   app.use(bodyParser.json()); // get our request parameters
   app.use(bodyParser.urlencoded({ extended: false }));
   //app.use(express.cookieParser()); // read cookies (needed for auth)
-  app.use(morgan('dev')); // use morgan to log requests to the console
+  //app.use(morgan('dev')); // use morgan to log requests to the console
   app.use( express.static(__dirname + "/../client") ); // default route
 }
