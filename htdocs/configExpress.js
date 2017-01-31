21eab2fb9b321e1c9c269fc6b99eb7cff4a462a8
deainya <deainya@gmail.com>
2017-02-01 01:29:23 +0400

    fix

--- htdocs/configExpress.js
+++ htdocs/configExpress.js
@@ -6,6 +6,6 @@ module.exports = function(app, express, bodyParser) {
   app.use(bodyParser.json()); // get our request parameters
   app.use(bodyParser.urlencoded({ extended: false }));
   //app.use(express.cookieParser()); // read cookies (needed for auth)
-  app.use(morgan('dev')); // use morgan to log requests to the console
+  //app.use(morgan('dev')); // use morgan to log requests to the console
   app.use( express.static(__dirname + "/../client") ); // default route
 }
