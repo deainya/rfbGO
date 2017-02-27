84caf67ae2da648d3db63d91e4fa119fd43310d3
deainya <deainya@gmail.com>
2017-02-27 08:23:03 +0400

    fix?

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,7 @@
 import angular from 'angular'
 import 'angular-ui-router'
 import 'angular-resource'
-import 'toastr'
+import 'angular-toastr'
 //import 'cordova' //ionic
 
 var Auth = require('./services/Auth');
