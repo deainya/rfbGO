191ec3f2ee3b58d9b68be9d9214b32bfad59b20c
deainya <deainya@gmail.com>
2017-03-05 23:48:12 +0400

    fix?

--- server/configExpress.js
+++ server/configExpress.js
@@ -7,9 +7,9 @@ module.exports = function(app, express, bodyParser) {
   app.use(bodyParser.urlencoded({ extended: false }));
   //app.use(express.cookieParser()); // read cookies (needed for auth)
   //app.use(morgan('dev')); // use morgan to log requests to the console
-  app.use(express.methodOverride());
+  //app.use(express.methodOverride());
 
   app.use( express.static(__dirname + "/../client") ); // default route
 
-  app.use(app.router);
+  //app.use(app.router);
 }
